8e463dbf5d735d18e087d2aea7c5dd21c6942de0
Chouichou <anna.bellec@wanadoo.fr>
2020-01-22 15:48:40 +0100

    Création dossier Vue

--- Modele/Utilisateurs/Utilisateur.cs
+++ Modele/Utilisateurs/Utilisateur.cs
@@ -8,6 +8,16 @@ namespace Utilisateurs
 {
     public abstract class Utilisateur : IUtilisateur
     {//Anna
+        private int Id_Util;
+        private string Nom;
+        private string Prenom;
+        private string Adresse;
+        private int Cp;
+        private string Ville;
+        private DateTime DateEmbauche;
+        private int Code_Secteur;
+        private int Code_Lab;
+
         public void Connecter()
         {
             
---
+++ Vue/Vue_Anna.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GSB_.Core.Vue
+{
+    /// <summary>
+    /// Interaction logic for Vue_Anna.xaml
+    /// </summary>
+    public partial class Vue_Anna : Window
+    {
+        public Vue_Anna()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
